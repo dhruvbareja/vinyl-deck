15ca2260c7b181e486701a32aca015d5a04c4071
dhruv <dhruvbareja17@gmail.com>
2025-09-28 05:58:23 +0530

    Initial Commit

---
+++ vinyl_app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  vinyl_app
+//
+//  Created by dhruv bareja on 9/28/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ vinyl_app/vinyl_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  vinyl_appApp.swift
+//  vinyl_app
+//
+//  Created by dhruv bareja on 9/28/25.
+//
+
+import SwiftUI
+
+@main
+struct vinyl_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
